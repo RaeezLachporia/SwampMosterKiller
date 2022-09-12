66307e7e70203a733993b043b6940094f50755be
RaeezLachporia <88308343+RaeezLachporia@users.noreply.github.com>
2022-09-12 20:24:51 +0200

    created game engine

---
+++ SwampMosterKiller/GameEngine.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwampMosterKiller
+{
+    class GameEngine :Map
+    {
+        private Map maps = new Map(5,8,3,9,6);
+
+        public GameEngine(int _minWidth, int _maxWidth, int _minHeight, int _maxHeight, int _numEnemy) : base(_minWidth, _maxWidth, _minHeight, _maxHeight, _numEnemy)
+        {
+            
+        }
+
+        public bool MovePlayer(Character.Movement movement)
+        {
+            return MovePlayer;
+        }
+    }
+}
